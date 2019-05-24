cc088bcef309ea5ec7650ced82091fd433278389
Matthew Reyes <43076895+keolasin@users.noreply.github.com>
2019-05-24 15:39:49 -0700

    add fs.writefile and fs.mkdir

--- terminal-commands.js
+++ terminal-commands.js
@@ -1,7 +1,25 @@
-const fs = require('fs');
+const fs = require('fs')
 
 module.exports.ls = () => {
   fs.readdir('./', (err, files) => {
+    const filesToString = files.reduce((acc, file) => {
+      return `${acc} ${file} `;
+    }, '');
 
+    console.log(filesToString);
   });
 };
+
+module.exports.touch = (fileName, content) => {
+  fs.writefile( '${fileName}', '${content}', error => {
+    if(error) throw error;
+    console.log(`File ${fileName} saved`);
+  };
+)};
+
+module.exports.mkdir = (path) => {
+  fs.mkdir('${path}', (error) => {
+    if (error) throw error;
+    console.log(`Folder made with path of ${path}`);
+  };
+)};
